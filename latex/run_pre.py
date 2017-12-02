6da5aed2e4bc8e21ece2ffa41a4b67030e864535
163 <xggong@163>
2017-12-02 13:05:01 +0800

    组会20171202pre

--- latex/run_pre.py
+++ latex/run_pre.py
@@ -2,7 +2,7 @@
 # @Author: YangZhou
 # @Date:   2017-06-21 01:41:49
 # @Last Modified by:   YangZhou
-# @Last Modified time: 2017-06-26 19:27:33
+# @Last Modified time: 2017-12-02 11:28:15
 import os
 if not os.path.exists("bin/"):
     os.mkdir("bin")
@@ -14,8 +14,6 @@ os.system("cp ../ref.bib .")
 os.system("cp ../../src/*.eps images/")
 os.system("cp ../*.tex .")
 os.system('xelatex presentation.tex')
-# os.system('xelatex presentation.tex')
-# os.system('bibtex presentation')
-# os.system('latex ../presentation.tex')
-# os.system('latex ../presentation.tex')
-# os.system("dvipdft presentation")
+os.system('bibtex presentation')
+os.system('xelatex ../presentation.tex')
+os.system("rm *.tex *.aux *.bbl *.blg *.dvi *.log *.bib *.out")
